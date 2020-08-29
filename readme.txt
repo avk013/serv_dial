b785472207370efe11fc4b89166427571315d4dd
Ol Kovalenko <avk013@gmail.com>
2020-08-29 21:18:59 +0300

    существенные интрументальные изменения, лгика не закончена

---
+++ readme.txt
@@ -0,0 +1,4 @@
+sc create AutoVPN start= auto binPath= "rasdial VPN логин пароль" DisplayName= "AutoVPN" depend=lanmanworkstation obj= "NT AUTHORITY\LocalService"
+
+
+sc delete AutoVPN
--- serv_dial/serv_dial.cs
+++ serv_dial/serv_dial.cs
@@ -32,7 +32,18 @@ namespace serv_dial
 
         }
         private void T2_Elapsed(object sender, EventArgs e)
-        {
+        {//
+            string my_ip = "172.16.137.1";
+            string ip1 = "172.16.137.11";
+            string ip2 = "172.16.137.12";
+            string ip3 = "172.16.137.13";
+            string wip1 = "154.41.5.12";
+            string wip2 = "91.240.97.145";
+            string wip3 = "193.160.225.58";
+            string nam1 = "pro1";
+            string nam2 = "pro2";
+            string nam3 = "pro3";
+
             //biblioteka.methodName();
             Ping pingSender = new Ping();
             PingOptions options = new PingOptions();
@@ -44,14 +55,34 @@ namespace serv_dial
             string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
             byte[] buffer = Encoding.ASCII.GetBytes(data);
             int timeout = 120;
-            IPAddress address = IPAddress.Parse("172.16.137.231");
+            IPAddress address = IPAddress.Parse(ip1);
             PingReply reply = pingSender.Send(address, timeout, buffer, options);
             if (reply.Status != IPStatus.Success)
             {
-                System.Diagnostics.Process.Start("c:/dial/dial.cmd");
-                System.Diagnostics.Process.Start("c:/dial/dial_.cmd");
+            command_(@"cmd.exe", @" /c c:\windows\system32\rasdial.exe global kil_rdp_servSM1 112267qwz >> C:\dial\in.txt");
+
+                // System.Diagnostics.Process.Start("rasdial",@" global kil_rdp_servSM1 112267qwz >> C:\dial\in.txt");
+                //              System.Diagnostics.Process.Start("c:/dial/dial.cmd");
+                //            System.Diagnostics.Process.Start("c:/dial/dial_.cmd");
             }
+
         }
+            private string command_(string FileName, string Arguments)
+            {
+                string output;
+                using (var p = new System.Diagnostics.Process())
+                {//узнаем PID сервиса AnyDesk
+                    p.StartInfo.FileName = FileName;
+                    p.StartInfo.Arguments = Arguments;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.StandardOutputEncoding = Encoding.GetEncoding("CP866");
+                    p.Start();
+                    output = p.StandardOutput.ReadToEnd();
+                }
+                 return output;
+            }
         protected override void OnStop()
         {
             System.Diagnostics.Process.Start("c:/dial/dial_.cmd");
